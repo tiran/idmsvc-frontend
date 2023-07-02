04ffbc4083e88a7e46962df7d2cb089aeddbc827
Alejandro Visiedo <avisiedo@redhat.com>
2023-07-02 10:45:03 +0200

    feat: set routes to local backend and chrome-service
    Add configuration to fec.config.js to proxy request to the local running
    backend and chrome-service for local development.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- fec.config.js
+++ fec.config.js
@@ -2,14 +2,27 @@ module.exports = {
   appUrl: ['/settings/hmsidm'],
   debug: true,
   useProxy: true,
+  useCloud: false,
   proxyVerbose: true,
   /**
    * Change to false after your app is registered in configuration files
    */
-  interceptChromeConfig: true,
+  // interceptChromeConfig: true,
+  interceptChromeConfig: false,
   /**
    * Add additional webpack plugins
    */
   plugins: [],
   _unstableHotReload: process.env.HOT === 'true',
+  routes: {
+    /* Add routes to the backend  */
+    '/api/hmsidm/': { host: 'http://localhost:8000' },
+    '/beta/api/hmsidm/': { host: 'http://localhost:8000' },
+    '/preview/api/hmsidm/': { host: 'http://localhost:8000' },
+
+    /* Add routes to the chrome-service*/
+    '/api/chrome-service/v1/static/': { host: 'http://localhost:9999' },
+    '/beta/api/chrome-service/v1/static/': { host: 'http://localhost:9999' },
+    '/preview/api/chrome-service/v1/static/': { host: 'http://localhost:9999' },
+  },
 };
