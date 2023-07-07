1fe66aa00eaad880559a7a2012c2a621a47143b6
Christian Heimes <cheimes@redhat.com>
2023-07-07 07:35:27 +0200

    test: add simple gitlab-ci pipeline
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- src/Components/DomainList/DomainList.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-
-// test('expect DomainList to render children', () => {
-//   const children = <h1>Hello</h1>;
-//   render(<DomainList>{children}</DomainList>);
-//   expect(screen.getByRole('heading')).toHaveTextContent('Hello');
-// });
