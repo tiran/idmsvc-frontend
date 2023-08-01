25e8caf2a42f2ff9a414d32eaf8b871f486867af
Alejandro Visiedo <avisiedo@redhat.com>
2023-08-01 14:42:13 +0200

    HMS-2089 fix: clean-up fec.config
    It is not necessary the paths for /beta or /preview for the backend or
    the chrome-service backend.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- fec.config.js
+++ fec.config.js
@@ -17,12 +17,8 @@ module.exports = {
   routes: {
     /* Add routes to the backend  */
     '/api/idmsvc/': { host: 'http://localhost:8000' },
-    '/beta/api/idmsvc/': { host: 'http://localhost:8000' },
-    '/preview/api/idmsvc/': { host: 'http://localhost:8000' },
 
     /* Add routes to the chrome-service*/
     '/api/chrome-service/v1/static/': { host: 'http://localhost:9999' },
-    '/beta/api/chrome-service/v1/static/': { host: 'http://localhost:9999' },
-    '/preview/api/chrome-service/v1/static/': { host: 'http://localhost:9999' },
   },
 };
