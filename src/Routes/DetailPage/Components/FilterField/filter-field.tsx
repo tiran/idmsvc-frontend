26316f07b77811d2867e7ee86019ab67a1439737
Alejandro Visiedo <avisiedo@redhat.com>
2023-11-09 02:04:27 +0100

    HMS-1623 feat: add filter component
    This change adds a composite component to enter the text to filter
    for the server tab in the detail view.
    
    We don't use this component (we decided to defer implementing it),
    but because this component was already written, we decided to commit
    it.  It is available for use at a later time.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

---
+++ src/Routes/DetailPage/Components/FilterField/filter-field.tsx
@@ -0,0 +1,79 @@
+import { Button, Dropdown, DropdownItem, DropdownToggle, InputGroup, TextInput } from '@patternfly/react-core';
+import React, { useState } from 'react';
+import { FilterIcon } from '@patternfly/react-icons/dist/esm/icons/filter-icon';
+import { SearchIcon } from '@patternfly/react-icons/dist/esm/icons/search-icon';
+
+/** Define the values for the dropdown component. */
+type FilterType = 'Name' | 'Location';
+
+/** Define the allowed properties for InputFilterServer component. */
+interface InputFilterServerProps {
+  /** Indicate the column to filter if any. */
+  column?: FilterType;
+  /** Indicate the value to filter for the selected column. */
+  value?: string;
+  /** Event to communicate a state change in the component. */
+  onChange?: (column: string, value: string) => void;
+}
+
+/** Component to let the user select the column to filter and the content to filter the table for. */
+export const InputFilterServer = (props: InputFilterServerProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [value, setValue] = useState<string>(props.value || '');
+  const [filter, setFilter] = useState<FilterType>(props.column || 'Name');
+
+  const onToggle = (isOpen: boolean) => {
+    setIsOpen(isOpen);
+  };
+
+  const onSelect = (event?: React.SyntheticEvent<HTMLDivElement>) => {
+    console.info('event.target.value=' + event?.target);
+    setIsOpen(false);
+  };
+
+  const dropdownItems = [
+    <DropdownItem
+      key="Name"
+      value="Name"
+      component="button"
+      onClick={() => {
+        setFilter('Name');
+      }}
+    >
+      Name
+    </DropdownItem>,
+    <DropdownItem
+      key="Location"
+      value="Location"
+      component="button"
+      onClick={() => {
+        setFilter('Location');
+      }}
+    >
+      Location
+    </DropdownItem>,
+  ];
+
+  const onChange = (value: string, event: React.FormEvent<HTMLInputElement>) => {
+    setValue(value);
+  };
+
+  return (
+    <>
+      <InputGroup>
+        <Dropdown
+          onSelect={onSelect}
+          toggle={
+            <DropdownToggle onToggle={onToggle} icon={<FilterIcon />}>
+              {filter}
+            </DropdownToggle>
+          }
+          isOpen={isOpen}
+          dropdownItems={dropdownItems}
+        />
+        <TextInput id="input-filter-dropdown" aria-label="input with dropdown and button" value={value} onChange={onChange} />
+        <Button id="input-filter-button" variant="control" icon={<SearchIcon />} />
+      </InputGroup>
+    </>
+  );
+};
