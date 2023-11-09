89d25f7cf81b09aad0d119c3fe75b62ff66b0c61
Alejandro Visiedo <avisiedo@redhat.com>
2023-11-09 22:11:26 +1000

    HMS-1623 test: initial for detail view
    Comment the currently not implemented unit tests for DetailGeneral and
    DetailServers components.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- src/Routes/DetailPage/Components/DetailGeneral/DetailGeneral.test.tsx
+++ src/Routes/DetailPage/Components/DetailGeneral/DetailGeneral.test.tsx
@@ -1,1 +1,16 @@
-@import '~@redhat-cloud-services/frontend-components-utilities/styles/variables';
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+// import { DetailGeneral } from './DetailGeneral';
+// import { AppContextProvider } from '../../../../AppContext';
+
+test('expect sample-component to render children', () => {
+  render(<></>);
+  // render(
+  //   <AppContextProvider>
+  //     <DetailGeneral />
+  //   </AppContextProvider>
+  // );
+  // expect(screen.getByRole('heading')).toHaveTextContent('Name');
+  // expect(screen.getByRole('heading')).toHaveTextContent('Location');
+});
--- src/Routes/DetailPage/Components/DetailServers/DetailServers.test.tsx
+++ src/Routes/DetailPage/Components/DetailServers/DetailServers.test.tsx
@@ -1,1 +1,10 @@
-@import '~@redhat-cloud-services/frontend-components-utilities/styles/variables';
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+// import { DetailServers } from './DetailServers';
+
+test('expect sample-component to render children', () => {
+  render(<></>);
+  // expect(screen.getByRole('heading')).toHaveTextContent('Name');
+  // expect(screen.getByRole('heading')).toHaveTextContent('Location');
+});
